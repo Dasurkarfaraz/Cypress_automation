62beb338e56e6ff6f983c849ff5728f40db28979
Faraz_Dasurkar <faraz.dasurkar@qualitykiosk.com>
2024-08-06 13:44:00 +0530

    Updated By Faraz Dasurkar

--- cypress/testcase/test1.js
+++ cypress/testcase/test1.js
@@ -8,7 +8,13 @@ it("testcase1",()=>{
     cy.get(".search-keyword").type("CA")
     cy.get(".product:visible").should('have.length',4)
     cy.get('.products').find('.product').should('have.length',4)
-    
+    cy.get('.products').find('.product').each(($el,index,$list) => {
+        const textelement = $el.find('h4.product-name').text()
+        if (textelement.contains("Cashwes")) {
+            cy.wrap($el).find('button').click()
+        }
+
+    })
 
 })
 

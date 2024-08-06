8afffbee19c92a5465f4b8224b98594fcc0ac37a
Faraz_Dasurkar <faraz.dasurkar@qualitykiosk.com>
2024-08-06 13:21:45 +0530

    Update as per product Find by

--- cypress/testcase/test1.js
+++ cypress/testcase/test1.js
@@ -7,6 +7,8 @@ it("testcase1",()=>{
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
     cy.get(".search-keyword").type("CA")
     cy.get(".product:visible").should('have.length',4)
+    cy.get('.products').find('.product').should('have.length',4)
+    
 
 })
 
